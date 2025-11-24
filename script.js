45ea910f41c15441addf494d40c9a1eb5ca931e1
Gazell27 <ranggagaga2765@gmail.com>
2025-11-25 02:42:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Data Analyst", "Leaderhip", "Business Intelegence"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
